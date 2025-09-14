d7940ccefa6502c6d6801cf0d522c14413b089e9
Pyoann <p_yoann@hotmail.com>
2025-09-14 15:33:25 -0400

    Delete spa directory

--- spa/components/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ===== FICHIER: components/footer.php =====
-?>
-<footer class="page-footer">
-    <div class="footer-content">
-        <h3>© 2025 Yoann Paul p_yoann@hotmail.com</h3>
-
-    </div>
-</footer>
--- spa/components/header.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ===== FICHIER: components/header.php  =====
-?>
-<header class="page-header">
-    <div class="header-content">
-        <h1>Espace notification, pub, autre...</h1>
-        <p>Section header - Réserver pour futur notification</p>
-    </div>
-</header>
